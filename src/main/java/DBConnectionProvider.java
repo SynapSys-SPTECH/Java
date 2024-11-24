b4d63b44f2c8f374b22b3990985b4e26776da042
brzenit <bryan.lima@sptech.school>
2024-11-24 05:08:03 -0300

    modificaçao conection

--- src/main/java/DBConnectionProvider.java
+++ src/main/java/DBConnectionProvider.java
@@ -9,9 +9,13 @@ public class DBConnectionProvider {
 
     public DBConnectionProvider() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/Synapsys");
-        basicDataSource.setUsername("root");
-        basicDataSource.setPassword("urubu100");
+//        basicDataSource.setUrl("jdbc:mysql://0.0.0.0:3306/Synapsys");
+//        basicDataSource.setUsername("root");
+//        basicDataSource.setPassword("urubu100");
+        basicDataSource.setUrl(System.getenv().getOrDefault("DB_URL", "jdbc:mysql://localhost:3306/Synapsys"));
+        basicDataSource.setUsername(System.getenv().getOrDefault("DB_USERNAME", "root"));
+        basicDataSource.setPassword(System.getenv().getOrDefault("DB_PASSWORD", "09241724"));
+
         this.dataSource = basicDataSource;
     }
 
