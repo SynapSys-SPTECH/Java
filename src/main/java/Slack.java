8acf37aa602db4ed50f49c686655b253a394e6cf
brzenit <bryan.lima@sptech.school>
2024-11-28 19:38:20 -0300

    ajustando classes slack

--- src/main/java/Slack.java
+++ src/main/java/Slack.java
@@ -1,90 +1,32 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
 
-import org.json.JSONObject;
-import org.springframework.jdbc.core.JdbcTemplate;
+public class NotificacaoSlack {
+    private static final String WEBHOOK_URL;
 
-public class Slack {
-
-    private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = System.getenv("https://hooks.slack.com/services/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");
-
-    // Conexão com o Slack
-    public static void conexao(JSONObject content) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder(
-                        URI.create(URL))
-                .header("accept", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println(String.format("Status: %s", response.statusCode()));
-        System.out.println(String.format("Response: %s", response.body()));
-    }
-
-    private void inserirNotificacaoBanco(JdbcTemplate connection, String titulo, String descricao) {
-        try {
-            DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String dataHora = LocalDateTime.now().format(formatoData);
-
-            String query = "INSERT INTO slack (titulo, descricao , updateAt, idteste) VALUES (?, ?, ?,?)";
-            int fkEmpresa = 1;
-
-            connection.update(query, titulo, descricao, dataHora, fkEmpresa );
-            System.out.println("Notificação inserida no banco de dados com sucesso!");
-
-        } catch (Exception e) {
-            System.out.println("Erro ao inserir notificação no banco de dados: ");
+    static {
+        WEBHOOK_URL = System.getenv("SLACK_URL");
+        if (WEBHOOK_URL == null || WEBHOOK_URL.isEmpty()) {
+            throw new IllegalArgumentException("A variável de ambiente SLACK_URL não foi definida.");
         }
     }
 
+    public static void EnviarNotificacaoSlack(String message) throws Exception {
+        String payload = "{\"text\":\"" + message + "\"}";
+        URL url = new URL(WEBHOOK_URL);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setDoOutput(true);
+        httpConn.setRequestMethod("POST");
+        httpConn.setRequestProperty("Content-Type", "application/json");
+        httpConn.setRequestProperty("Accept", "application/json");
 
-
-    //notificação do total de linhas inseridas
-    public void gerarNotificacaoLinhas(JdbcTemplate connection) {
-        String query = "SELECT COUNT(*) AS total_linhas FROM Synapsys.leitura";
-
-        List<Map<String, Object>> resultados = connection.queryForList(query);
-
-        if (!resultados.isEmpty()) {
-            Long totalLinhas = (Long) resultados.get(0).get("total_linhas");
-            String descricao = "Total de linhas inseridas: " + totalLinhas;
-
-            try {
-                JSONObject json = criarJson("Linhas inseridas", descricao);
-                inserirNotificacaoBanco(connection, "Linhas inseridas", descricao);
-
-                conexao(json);
-            } catch (Exception e) {
-                System.out.println("Erro ao realizar Query" + e.getMessage());
-            }
-
+        try (OutputStream os = httpConn.getOutputStream()) {
+            byte[] input = payload.getBytes("utf-8");
+            os.write(input, 0, input.length);
         }
-    }
 
-
-    private JSONObject criarJson(String titulo, String descricao) {
-        JSONObject json = new JSONObject();
-
-        json.put("text", "*" + titulo + "*\n" + descricao);
-
-        return json;
-    }
-
-    public void executarNotificacoes() {
-        DBConnectionProvider db = new DBConnectionProvider();
-        JdbcTemplate connection = db.getConnection();
-
-        gerarNotificacaoLinhas(connection);
+        int responseCode = httpConn.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
     }
 }
-
-
